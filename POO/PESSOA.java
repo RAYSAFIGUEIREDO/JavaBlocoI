39ada802bce572d4d21ea9ee94abbfb4f88d389b
Raysa Figueiredo <98346079+raysafiigueiredo@users.noreply.github.com>
2024-02-26 12:11:31 -0300

    Update PESSOA.java

--- POO/PESSOA.java
+++ POO/PESSOA.java
@@ -1,30 +1,19 @@
 package POO;
 
-public class PESSOA{
-	private String PrimeiroNome;
-	private String UltimoNome;
-	private String NumeroCpf;
-	
-	
-	
-	public PESSOA ("String Primeiro, String Ultimo");
-	{	
-
-		 PrimeiroNome = Primeiro;
-		 UltimoNome = Ultimo;
-		
-	}
-
-     public String getNomeCompleto ()
-     {
+public class PESSOA {
+    private String PrimeiroNome;
+    private String UltimoNome;
+    private String NumeroCpf;
     
-    	      
-    String NomeCompleto = ("Raysa Figueiredo");
-return NomeCompleto;    	
-     
-  
-    	
-    	
-	}
+    // Correção do construtor
+    public PESSOA(String Primeiro, String Ultimo) {
+        PrimeiroNome = Primeiro;
+        UltimoNome = Ultimo;
+    }
+
+    // Método para retornar o nome completo utilizando os atributos PrimeiroNome e UltimoNome
+    public String getNomeCompleto() {
+        return PrimeiroNome + " " + UltimoNome;
+    }
 }
 
